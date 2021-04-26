b6339c6b50a2224e8ffd304a607209c287aba538
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-26 17:53:42 +0300

    select stylization with jquery selectric

--- summer-trip-2021/scripts/indexPage.js
+++ summer-trip-2021/scripts/indexPage.js
@@ -1,6 +1,3 @@
-const descImg = document.querySelector('.desc-img');
-document.body.addEventListener('click', event => {
-    console.log('click');
-    console.log(event);
-});
-
+$(function() {
+    $('select').selectric();
+  });
