bc97d78951b8ed2cc84bb88b5d825f9b92c17a4c
Daria Kozina <kozina.flowers@yandex.ru>
2021-05-02 23:33:55 +0300

    showInfo on select change

--- summer-trip-2021/scripts/indexPage.js
+++ summer-trip-2021/scripts/indexPage.js
@@ -1,6 +1,26 @@
-$(function() {
+document.addEventListener('DOMContentLoaded', function() {
+  //jquery for select
+  $(function() {
     $('select').selectric({
       disableOnMobile: false,
       nativeOnMobile: false
     });
   });
+
+  //show info about characters
+  const showInfo = () => {
+    const charactersInfoItems = document.querySelectorAll('.characters-info__item');
+    $('select').on('selectric-change', () => {
+      const reg = new RegExp(`^${$('select').val()}`);
+      charactersInfoItems.forEach(item => {
+        const regCheck = reg.test(item.textContent.trim());
+        item.classList.add('o-none');
+        if (regCheck) {
+          item.classList.remove('o-none');
+        }
+      })
+    })
+  };
+
+  showInfo();
+});
