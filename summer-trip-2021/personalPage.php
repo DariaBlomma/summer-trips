3c7d9cfc6a17d0d8bf7a93ccd626a8ce2b389f05
Daria Kozina <kozina.flowers@yandex.ru>
2021-07-15 13:36:52 +0300

    login works from index page with php session

--- summer-trip-2021/personalPage.php
+++ summer-trip-2021/personalPage.php
@@ -1,92 +1,64 @@
-<!-- чтобы создать базы данных:
-1.Включить в настройках нужный модуль
-2. Логин root, пароль root
-3. Проверить вход можно через adminer - дополнительно -->
 <?php
+session_start();
+$_SESSION['id'] = session_id();
+$id = $_SESSION['id'];
+
 // // get json
 $json = file_get_contents('php://input');
 // // data is object
 $data = json_decode($json);
 
-	//Устанавливаем доступы к базе данных:
-        $host = '127.0.0.1';
-		//  $host = 'localhost'; //имя хоста, на локальном компьютере это localhost
-		$user = 'root'; //имя пользователя, по умолчанию это root
-		$password = 'root'; //пароль, по умолчанию пустой
-		$db_name = 'avengers'; //имя базы данных
+//Устанавливаем доступы к базе данных:
+$host = '127.0.0.1';
+//  $host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+$user = 'root'; //имя пользователя, по умолчанию это root
+$password = 'root'; //пароль, по умолчанию пустой
+$db_name = 'avengers'; //имя базы данных
 
-        // для хостинга спринтхост
-        // $host = 'localhost'; 
-		// $user = 'f0497458_root'; 
-		// $password = 'root'; 
-		// $db_name = 'f0497458_avengers'; 
+// для хостинга спринтхост
+// $host = 'localhost'; 
+// $user = 'f0497458_root'; 
+// $password = 'root'; 
+// $db_name = 'f0497458_avengers'; 
 
-	//Соединяемся с базой данных используя наши доступы:
-        $mysqli = new mysqli($host, $user, $password, $db_name);
+//Соединяемся с базой данных используя наши доступы:
+$mysqli = new mysqli($host, $user, $password, $db_name);
 
-    // if ($mysqli->connect_errno) {
-    //     echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    // }
-    // echo $mysqli->host_info . "\n";
+$mysqli->query("CREATE TABLE IF NOT EXISTS players(
+    unique_id VARCHAR(50),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    PRIMARY KEY (unique_id)
+    )");
+$mysqli->query("CREATE TABLE IF NOT EXISTS points(
+    lesson_id INT AUTO_INCREMENT,
+    player_id VARCHAR(50),
+    lesson_number INT,
+    lesson_points INT,
+    homework INT,
+    per_page INT,
+    additional_tasks INT,
+    PRIMARY KEY (lesson_id),
+    UNIQUE (player_id, lesson_number),
+    FOREIGN KEY (player_id) REFERENCES players (unique_id) 
+    )");
 
-    $mysqli->query("CREATE TABLE IF NOT EXISTS players(
-        -- person_id INT AUTO_INCREMENT,
-        unique_id VARCHAR(10),
-        first_name VARCHAR(20),
-        last_name VARCHAR(20),
-        
-        -- PRIMARY KEY (person_id)
-        PRIMARY KEY (unique_id)
-        )");
-    $mysqli->query("CREATE TABLE IF NOT EXISTS points(
-        lesson_id INT AUTO_INCREMENT,
-        -- player_id INT,
-        player_id VARCHAR(10),
-        lesson_number INT,
-        lesson_points INT,
-        homework INT,
-        per_page INT,
-        additional_tasks INT,
-        PRIMARY KEY (lesson_id),
-        UNIQUE (player_id, lesson_number),
-        -- FOREIGN KEY (player_id) REFERENCES players (person_id)
-        FOREIGN KEY (player_id) REFERENCES players (unique_id) 
-        )");
-    // $mysqli->query("CREATE TABLE current(
-    //     id INT,
-    //     user VARCHAR(20),
-    //     PRIMARY KEY (id)
-    //     )");
-    // $mysqli->query("INSERT INTO current(id) VALUES (1)");
-// $result = 0;
-    if ($data !== null) {
-    // обратились к свойству объекта
-        $name = $data->real_name;
-        $coins = $data->coins;
-        $id = $data->id;
-        $lesson = $data->lesson;
-    // записываем игрока и баллы со страницы при его регистрации
-        // $mysqli->query("INSERT INTO players(first_name, unique_id) VALUES ('$name', '$id')");
-            $mysqli->query("INSERT INTO players(unique_id, first_name) VALUES ('$id', '$name') 
-            ON DUPLICATE KEY UPDATE unique_id='$id', first_name='$name'");
-    // current user
-        // $mysqli->query("UPDATE current SET id = 1, user = '$name'");
-
-        // $mysqli->query("INSERT INTO points(player_id, lesson_number, per_page) VALUES
-        // ((SELECT person_id FROM players WHERE first_name = '$name'), 1, '$coins')");
-        // $mysqli->query("INSERT INTO points(player_id, lesson_number, per_page) VALUES
-        // ((SELECT person_id FROM players WHERE unique_id = '$id'), '$lesson', '$coins')");
-            
-            $mysqli->query("INSERT INTO points(player_id, lesson_number, per_page) VALUES
-            ((SELECT unique_id FROM players WHERE unique_id = '$id'), 1, '$coins')
-            ON DUPLICATE KEY UPDATE player_id='$id', lesson_number=1, per_page='$coins'");
-
-// $result = $mysqli->query("SELECT lesson_number, lesson_points, homework, per_page, additional_tasks 
-// FROM points 
-// WHERE player_id = 
-// (SELECT person_id FROM players WHERE unique_id = '$id')");
-    }
+if ($data !== null) {
+// обратились к свойству объекта
+    $name = $data->real_name;
+    $coins = $data->coins;
+    // $id = $data->id;
+    $lesson = $data->lesson;
+// записываем игрока и баллы со страницы при его регистрации
+    $mysqli->query("INSERT INTO players(unique_id, first_name) VALUES ('$id', '$name') 
+    ON DUPLICATE KEY UPDATE unique_id='$id', first_name='$name'");
+    
+    $mysqli->query("INSERT INTO points(player_id, lesson_number, per_page) VALUES
+    ((SELECT unique_id FROM players WHERE unique_id = '$id'), 1, '$coins')
+    ON DUPLICATE KEY UPDATE player_id='$id', lesson_number=1, per_page='$coins'");
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -108,7 +80,6 @@ $data = json_decode($json);
             </div>
             <div class="timer-action">
                 Time to win
-
             </div>
             <div class="timer-numbers" id="timer">
                 <span id="timer-hours">19</span>
@@ -153,13 +124,7 @@ $data = json_decode($json);
                             <!-- <th class='total'>Total</th> -->
                         </tr>
                     </thead>
-                    <?php 
-                // get json
-// $json = file_get_contents('php://input');
-// var_dump($json);
-// // data is object
-// $data = json_decode($json);
-// var_dump($data);
+<?php 
 if($_SERVER["REQUEST_METHOD"]=="POST") {
     echo 'post ';
     var_dump($data);
@@ -168,59 +133,27 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
     echo 'not post';
     var_dump($data);
 }
-                 ?>
+echo $id;
+?>
                     <!-- <tr> -->
                         <!-- <th class='number'>1</th> -->
 <?php
-    // $result = $mysqli->query("SELECT lesson_number, lesson_points, homework, per_page, additional_tasks 
-    // FROM points 
-    // WHERE player_id = 
-    // (SELECT person_id FROM players WHERE first_name = 
-    // (SELECT user FROM current)
-    // )");
-    if ($data !== null) {
-        // обратились к свойству объекта
-            // $name = $data->real_name;
-            // $coins = $data->coins;
-            $id = $data->id;
-            // $lesson = $data->lesson;
-        // записываем игрока и баллы со страницы при его регистрации
-            // $mysqli->query("INSERT INTO players(unique_id, first_name) VALUES ('$id', '$name') 
-            //     ON DUPLICATE KEY UPDATE unique_id='$id', first_name='$name'");
-        // current user
-            // $mysqli->query("UPDATE current SET id = 1, user = '$name'");
-    
-            // $mysqli->query("INSERT INTO points(player_id, lesson_number, per_page) VALUES
-            // ((SELECT person_id FROM players WHERE first_name = '$name'), 1, '$coins')");
-            // $mysqli->query("INSERT INTO points(player_id, lesson_number, per_page) VALUES
-            //  ((SELECT unique_id FROM players WHERE unique_id = '$id'), 1, '$coins')");
-            // $mysqli->query("INSERT INTO points(player_id, lesson_number, per_page) VALUES
-            // ((SELECT unique_id FROM players WHERE unique_id = '$id'), 1, '$coins')
-            // ON DUPLICATE KEY UPDATE player_id='$id', lesson_number=1, per_page='$coins'");
-    
-    // $result = $mysqli->query("SELECT lesson_number, lesson_points, homework, per_page, additional_tasks 
-    // FROM points 
-    // WHERE player_id = 
-    // (SELECT person_id FROM players WHERE unique_id = '$id')");
-    $result = $mysqli->query("SELECT lesson_number, lesson_points, homework, per_page, additional_tasks 
-    FROM points 
-    WHERE player_id = 
-    (SELECT unique_id FROM players WHERE unique_id = '$id')");
-        $row = $result->fetch_assoc();
-        foreach ($result as $row) {
-            echo "
-            <tr>
-                <td class='number'>". $row['lesson_number'] . "</td>
-                <td class='to-count'>". $row['lesson_points'] . "</td>
-                <td class='to-count'>". $row['homework'] . "</td>
-                <td class='to-count'>". $row['per_page'] . "</td>
-                <td class='to-count'>". $row['additional_tasks'] . "</td>
-            </tr>";
-        }
-    }
+$result = $mysqli->query("SELECT lesson_number, lesson_points, homework, per_page, additional_tasks 
+FROM points 
+WHERE player_id = 
+(SELECT unique_id FROM players WHERE unique_id = '$id')");
 
-    
-// mysqli_close($mysqli);
+$row = $result->fetch_assoc();
+foreach ($result as $row) {
+    echo "
+    <tr>
+        <td class='number'>". $row['lesson_number'] . "</td>
+        <td class='to-count'>". $row['lesson_points'] . "</td>
+        <td class='to-count'>". $row['homework'] . "</td>
+        <td class='to-count'>". $row['per_page'] . "</td>
+        <td class='to-count'>". $row['additional_tasks'] . "</td>
+    </tr>";
+}
 ?>
                         <!-- <th>12</th>
                         <th>10</th>
